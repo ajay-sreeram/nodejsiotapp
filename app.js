75a594866a0223faf8ec908f00c4619a9a82d32a
timesplice <timesplicemobile@gmail.com>
2017-03-23 16:44:02 +0530

    revoking to previous code with removing bodyparser

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
+
 
 
 var app = express();
-app.use(bodyParser.json());
 
 
 app.get('/*', function (req, res) {
